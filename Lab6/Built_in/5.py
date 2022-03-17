1ba493f161d1a616e5b8427794f8a536acc6a5e2
Serikbolsyn <84507955+Samatuly@users.noreply.github.com>
2022-03-17 17:31:34 +0600

    Delete 5.py

--- Lab6/Built_in/5.py
+++
@@ -1,9 +0,0 @@
-mytuple = ()
-num = int(input("Please, write number of items in tuple: "))
-l = list(mytuple)
-i = 0
-while i < num:
-    a = input()
-    l.append(a)
-    i += 1
-print(all(tuple(l)))
